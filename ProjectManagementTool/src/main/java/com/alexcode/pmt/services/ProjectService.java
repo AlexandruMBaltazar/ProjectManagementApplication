56c35c6c32c387e0ad48d8ce5bf3ea721789081e
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2020-12-14 14:19:48 +0000

    Create CRUD for project entity

--- ProjectManagementTool/src/main/java/com/alexcode/pmt/services/ProjectService.java
+++ ProjectManagementTool/src/main/java/com/alexcode/pmt/services/ProjectService.java
@@ -26,4 +26,28 @@ public class ProjectService {
             throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
         }
     }
+
+    public Project findProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Project with ID '"+projectId+"' can not be deleted");
+        }
+
+        projectRepository.delete(project);
+    }
 }
