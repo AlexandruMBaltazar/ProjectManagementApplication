a124336de89b47af7ec99af76ec0474a35073352
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2020-12-22 11:54:19 +0000

    Add Backlog and ProjectTask entities

---
+++ ProjectManagementTool/src/main/java/com/alexcode/pmt/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package com.alexcode.pmt.repositories;
+
+import com.alexcode.pmt.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+}
