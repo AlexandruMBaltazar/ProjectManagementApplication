3c0fb86e5e3490155c338ee7857d9c3c3e4ab796
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2021-01-05 16:41:26 +0000

    Routes to ProjectBoard and AddProjectTask

--- project-management-tool-portal/src/reducers/index.js
+++ project-management-tool-portal/src/reducers/index.js
@@ -2,8 +2,6 @@ import {combineReducers} from 'redux';
 import backlogReducer from './backlogReducer';
 import errorReducer from './errorReducer';
 import projectReducer from './projectReducer';
-import backlogReducer from './backlogReducer';
-
 
 export default combineReducers ({
     errors: errorReducer,
