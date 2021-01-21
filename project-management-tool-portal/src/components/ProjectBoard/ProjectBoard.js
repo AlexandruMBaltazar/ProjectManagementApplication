b0cf8e5dbed4f7cee2826c3beccf763c0e45f892
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2021-01-21 14:52:22 +0000

    Display backlokg with error handling

--- project-management-tool-portal/src/components/ProjectBoard/ProjectBoard.js
+++ project-management-tool-portal/src/components/ProjectBoard/ProjectBoard.js
@@ -7,15 +7,53 @@ import { getBacklog } from "../../actions/backlogActions";
 
 class ProjectBoard extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            errors: {}
+        }
+    }
+
+
+    static getDerivedStateFromProps(nextProps) {
+        if (nextProps.errors) {
+            return ({ errors: nextProps.errors });
+        }
+
+        return null;
+    }
+
     componentDidMount() {
         const {id} = this.props.match.params;
         this.props.getBacklog(id);
     }
 
+    displayBoard = (errors, project_tasks) => {
+        if (project_tasks.length < 1) {
+            if (errors.projectNotFound) {
+                return (
+                    <div className="alert alert-danger text-center" role="alert" >
+                        {errors.projectNotFound}
+                    </div>
+                );
+            } else {
+                return (
+                    <div className="alert alert-info text-center" role="alert" >
+                        No Project Tasks On This Board
+                    </div>
+                );    
+            }
+        } else {
+           return (<Backlog project_tasks_prop = {project_tasks}/>);
+        }
+    };
+
 
     render() {
         const {id} = this.props.match.params;
         const {project_tasks} = this.props.backlog;
+        const {errors} = this.state;
+
         return (
             <div className="container">
                 <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
@@ -23,7 +61,7 @@ class ProjectBoard extends Component {
                 </Link>
                 <br />
                 <hr />
-                <Backlog project_tasks_prop = {project_tasks}/>
+                {this.displayBoard(errors, project_tasks)}
             </div>
     
         )
@@ -32,11 +70,14 @@ class ProjectBoard extends Component {
 
 ProjectBoard.propTypes = {
     backlog: PropTypes.object.isRequired,
-    getBacklog: PropTypes.func.isRequired
+    getBacklog: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired,
+
   };
   
   const mapStateToProps = state => ({
-    backlog: state.backlog
+    backlog: state.backlog,
+    errors: state.errors
   });
   
   export default connect(
