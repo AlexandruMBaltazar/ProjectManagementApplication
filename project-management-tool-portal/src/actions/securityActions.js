831d1c3e44e8ab9a5a90be00f6411e3d8127157b
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2021-02-11 17:42:03 +0000

    Allow user to login

--- project-management-tool-portal/src/actions/securityActions.js
+++ project-management-tool-portal/src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import setJWTHeader from '../securityUtils/SetJWTHeader';
+import jwt_decode from "jwt-decode";
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 export const createNewUser = (newUser, history) => async dispatch => {
     try {
@@ -19,4 +21,37 @@ export const createNewUser = (newUser, history) => async dispatch => {
         })
     }
 
-}
+};
+
+export const login = loginRequest => async dispatch => {
+
+    try {
+        //Make the login post request 
+        const res = await axios.post('/api/users/login', loginRequest);
+
+        //extract the token from the response
+        const {token} = res.data;
+
+        //store the token in the localStorage
+        localStorage.setItem("jwtToken", token);
+
+        //set the token in the headers - important
+        setJWTHeader(token);
+
+        //decode the token to get the payload
+        const decoded = jwt_decode(token); 
+
+        //dispatch to the securityReducer
+        dispatch({
+            type: SET_CURRENT_USER,
+            payload: decoded
+        });
+
+    } catch (err) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    }
+
+};
--- project-management-tool-portal/src/components/UserManagement/Login.js
+++ project-management-tool-portal/src/components/UserManagement/Login.js
@@ -1,19 +1,94 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {login} from '../../actions/securityActions';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import {connect} from 'react-redux';
 
 class Login extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            username: "",
+            password: "",
+            errors: {}
+        }
+
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.security.validToken !== prevProps.security.validToken) {
+          this.props.history.push("/dashboard");
+        }
+    }
+
+    static getDerivedStateFromProps(nextProps) {
+        if (nextProps.errors) {
+            return ({ errors: nextProps.errors });
+        }
+
+        return null;
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+    
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const loginRequest = {
+            username: this.state.username,
+            password: this.state.password
+        };
+
+        this.props.login(loginRequest);
+    }
+
+
     render() {
+        const {errors} = this.state;
+
         return (
             <div className="login">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-8 m-auto">
                             <h1 className="display-4 text-center">Log In</h1>
-                            <form action="dashboard.html">
+                            <form onSubmit={this.onSubmit}>
                                 <div className="form-group">
-                                    <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" />
+                                    <input
+                                        type="email" 
+                                        placeholder="Email Address - Username" 
+                                        name="username" 
+                                        value={this.state.username}
+                                        onChange={this.onChange} 
+                                        className={classnames("form-control form-control-lg",{
+                                            "is-invalid": errors.username
+                                        })}                                         
+                                     />
+                                     {errors.username && (
+                                         <div className="invalid-feedback">{errors.username}</div>
+                                     )}                                     
                                 </div>
                                 <div className="form-group">
-                                    <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" />
+                                    <input 
+                                        type="password" 
+                                        placeholder="Password" 
+                                        name="password" 
+                                        value={this.state.password}
+                                        onChange={this.onChange}
+                                        className={classnames("form-control form-control-lg",{
+                                            "is-invalid": errors.password
+                                        })}  
+                                    />
+                                     {errors.password && (
+                                         <div className="invalid-feedback">{errors.password}</div>
+                                     )}                                      
                                 </div>
                                 <input type="submit" className="btn btn-info btn-block mt-4" />
                             </form>
@@ -25,4 +100,14 @@ class Login extends Component {
     }
 }
 
-export default Login;
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+    security: state.security,
+    errors: state.errors
+})
+
+export default connect(mapStateToProps, {login})(Login);
